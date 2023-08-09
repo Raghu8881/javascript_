2b5795a9bd3cd04b7884c38ec95cb97117233095
Raghu Prasad <63818961+Raghu8881@users.noreply.github.com>
2023-08-09 17:59:56 +0000

    Operatins done

---
+++ 01_Basics_JS/04_Operations.js
@@ -0,0 +1,21 @@
+let value=88
+let negativeValue=-value//here the value will be printed with negative
+console.log(negativeValue)//-88 will be printed
+
+ console.log(2+2);
+ console.log(2-2);
+ console.log(2*2);
+ console.log(2**3);
+ console.log(2/3);
+ console.log(2%3);
+
+
+ let str1="Hello"
+ let str2=" Puppy"
+ console.log(str1+str2)
+
+ console.log(81+"88")
+ console.log("88"+81)
+ console.log("88"+"81")
+ console.log("88"+ 81 + 1)
+ console.log(88 + 81 + "12")//add 1st and 2nd term and the join with number 12
